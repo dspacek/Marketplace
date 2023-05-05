273dc45e201bbd254af2e3e495b4934630bf40bd
David <david.spacek@ucdenver.edu>
2023-05-05 16:52:42 -0600

    Update

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ const config = {
   jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
   mongoUri: process.env.MONGODB_URI ||
     process.env.MONGO_HOST ||
-      'mongodb+srv://node1:apigee@finalproject.j0dp51a.mongodb.net/api?retryWrites=true&w=majority',
+      'mongodb+srv://dspacek:hQOGtQNPH6SpFBm5@moviesdb.i9hcac4.mongodb.net/api?retryWrites=true&w=majority',
    
   // stripe_connect_test_client_id: 'YOUR_stripe_connect_test_client',
   // stripe_test_secret_key: 'YOUR_stripe_test_secret_key',
