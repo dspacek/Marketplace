5154b9e30074c17a5efb7b044dadfac04a5cde47
David <david.spacek@ucdenver.edu>
2023-05-06 00:43:38 -0600

    Update

--- client/auction/NewAuction.js
+++ client/auction/NewAuction.js
@@ -90,12 +90,11 @@ export default function NewAuction() {
       values.startingBid && auctionData.append('startingBid', values.startingBid)
       values.bidStart && auctionData.append('bidStart', values.bidStart)
       values.bidEnd && auctionData.append('bidEnd', values.bidEnd)
-      
       create({
         userId: jwt.user._id
       }, {
         t: jwt.token
-      }, console.log("auctionData", auctionData), console.log("data", data), auctionData).then((data) => {
+      }, auctionData).then((data) => {
         if (data.error) {
           setValues({...values, error: data.error})
         } else {
